a9b7083bf5272614673d2da269a57c5f9880affb
root <root@localhost.localdomain>
2023-08-15 16:07:43 +0530

    initial commit

---
+++ new.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+read -p "Enter your name: " name
+read -p "Enter your surname: " sur
+
+echo "Your name is: $name $sur"
---
+++ new2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+read -p "Enter the first number: " num1
+read -p "Enter the sencond number: " num2
+add=$[$num1 + $num2]
+echo "The addition of two digit is: $add"
+
